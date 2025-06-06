bd096844e7fbcf95b9a640948e59b3ebeac1264f
holly_sizzle <shizuru@elisa.biz>
2025-06-06 13:12:31 +0000

    13.4 done

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -47,4 +47,20 @@ class MicropostsInterfaceTest < MicropostsInterface
     get user_path(users(:archer))
     assert_select 'a', { text: 'delete', count: 0 }
   end
+
+  test 'should have a file input field for images' do
+    get root_path
+    assert_select 'input[type=file]'
+  end
+
+  test 'should be able to attach an image' do
+    content = 'This micropost really ties the room together'
+    img = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: content,
+                                                 image: img } }
+    assert assigns(:micropost).image.attached?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+  end
 end
