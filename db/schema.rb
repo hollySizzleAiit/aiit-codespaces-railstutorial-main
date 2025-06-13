c6e45ed894f9c8471687fd1563ee87ad8c11cb0e
holly_sizzle <shizuru@elisa.biz>
2025-06-13 21:23:46 +0900

    エラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_06_08_152632) do
+ActiveRecord::Schema[7.0].define(version: 2025_06_08_165448) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_08_152632) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.integer "followers_count", default: 0, null: false
+    t.integer "following_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
